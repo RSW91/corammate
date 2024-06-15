10e866c774646b9fcb815505cb0cef84dd9f8c6d
Ryusungwoo <ryusw91rt@gmail.com>
2024-06-15 13:17:36 +0900

    Add authentication and user registration features

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,3 +27,24 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: 'Something went wrong' });
   }
 };
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
